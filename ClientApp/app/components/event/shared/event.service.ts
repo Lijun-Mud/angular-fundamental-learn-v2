7639db51d9d95c44411f5eab57389adb91e1185c
Lijun <mudfans999@live.com>
2017-08-28 08:27:06 +0800

    add images for app

--- ClientApp/app/components/event/shared/event.service.ts
+++ ClientApp/app/components/event/shared/event.service.ts
@@ -24,7 +24,7 @@ const Events  = [
         date: '9/26/2036',
         time: '10:00 am',
         price: 599.99,
-        imageUrl: '/app/assets/images/angularconnect-shield.png',
+        imageUrl: '/assets/images/angularconnect-shield.png',
         location: {
             address: '1057 DT',
             city: 'London',
@@ -102,7 +102,7 @@ const Events  = [
         date: '4/15/2037',
         time: '9:00 am',
         price: 950.00,
-        imageUrl: '/app/assets/images/ng-nl.png',
+        imageUrl: '/assets/images/ng-nl.png',
         onlineUrl: 'http://ng-nl.org/',
         sessions: [
             {
@@ -158,7 +158,7 @@ const Events  = [
         date: '5/4/2037',
         time: '9:00 am',
         price: 759.00,
-        imageUrl: '/app/assets/images/ng-conf.png',
+        imageUrl: '/assets/images/ng-conf.png',
         location: {
             address: 'The Palatial America Hotel',
             city: 'Salt Lake City',
@@ -240,7 +240,7 @@ const Events  = [
         date: '6/10/2037',
         time: '8:00 am',
         price: 800.00,
-        imageUrl: '/app/assets/images/basic-shield.png',
+        imageUrl: '/assets/images/basic-shield.png',
         location: {
             address: 'The UN Angular Center',
             city: 'New York',
@@ -289,7 +289,7 @@ const Events  = [
         date: '2/10/2037',
         time: '9:00 am',
         price: 400.00,
-        imageUrl: '/app/assets/images/ng-vegas.png',
+        imageUrl: '/assets/images/ng-vegas.png',
         location: {
             address: 'The Excalibur',
             city: 'Las Vegas',
